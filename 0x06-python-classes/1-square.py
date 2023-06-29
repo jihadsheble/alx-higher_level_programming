281fbe3afc87be429ec84d18fc1bf4d2385d9d5e
jihadsheble <jihadsheble@gmail.com>
2023-06-29 10:47:06 -0700

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a class Square that defines a square."""
+
+
+class Square:
+    """A class Square that defines a square."""
+    def __init__(self, size):
+    """Initializes the Square instance with a size."""
+        self.__size = size
